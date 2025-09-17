e825ec73fbe0358048259cc9cb41c2106c22228f
H82M19\Student <Student>
2025-09-17 08:49:52 +0700

    bài thực hành tuần 05 làm ở lớp

---
+++ 22718451_NguyenHoangKhang_Tuan05/App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+// or any files within the Snack
+import ShopList from "./components/ShopList.jsx"
+
+export default function App() {
+  return <ShopList/>
+}
+
+
